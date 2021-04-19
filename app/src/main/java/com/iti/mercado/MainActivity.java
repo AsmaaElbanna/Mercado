36d1fc66270bdc2e90110bbec9d1e3c130f88d8b
Amr <amr.zx35@gmail.com>
2021-04-19 03:28:52 +0200

    log in & sign up
    V 0.0.1

--- app/src/main/java/com/iti/mercado/MainActivity.java
+++ app/src/main/java/com/iti/mercado/MainActivity.java
@@ -2,13 +2,31 @@ package com.iti.mercado;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int TimeSplash = 5000 ;  // 1000 = 1 sec
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // to  hide action bar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent  = new Intent(MainActivity.this , LoginActivity.class)  ;
+                startActivity(homeIntent);
+                finish();
+            }
+        },TimeSplash) ;
+
     }
 }
